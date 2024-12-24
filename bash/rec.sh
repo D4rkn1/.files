6d33183a074af4c499601e8edc8c92f21372c025
khaos0130 <73800712+khaos0130@users.noreply.github.com>
2024-12-24 12:06:39 +0800

    rec.sh: include alsa output monitor

--- bash/rec.sh
+++ bash/rec.sh
@@ -5,4 +5,4 @@ pgrep -x "wf-recorder" && pkill -INT -x wf-recorder && notify-send -h string:wf-
 notify-send -h string:wf-recorder:record -t 100 "Recording"  -u low
 
 dateTime=$(date +%m-%d-%Y-%H:%M:%S)
-wf-recorder --bframes max_b_frames -f $HOME/Recordings/$dateTime.mp4
+wf-recorder -a alsa_output.pci-0000_05_00.6.analog-stereo.monitor --bframes max_b_frames -f $HOME/Recordings/$dateTime.mp4
