ff41d4c30ee6d5ff461158979908ba4a6c2bc22e
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2025-08-17 21:22:34 +0800

    sul.sh: remove new line on copied url

--- bash/sul.sh
+++ bash/sul.sh
@@ -3,10 +3,10 @@ key=$(< $HOME/secrets/sul)
 
 if [ $# -ge 1 ]; then
     for file in "$@"; do
-        curl -s -F "file=@$file" "https://s-ul.eu/api/v1/upload?wizard=true&key=$key" | jq -r '.url' | xargs | wl-copy
+        curl -s -F "file=@$file" "https://s-ul.eu/api/v1/upload?wizard=true&key=$key" | jq -r '.url' | tr -d '\n' | wl-copy
         notify-send -u low "up" 
     done
 else
-    slurp | grim -g - - | curl -s -F "file=@-;filename=screenshot.png" "https://s-ul.eu/api/v1/upload?wizard=true&key=$key" | jq -r '.url' | wl-copy
+    slurp | grim -g - - | curl -s -F "file=@-;filename=screenshot.png" "https://s-ul.eu/api/v1/upload?wizard=true&key=$key" | jq -r '.url' | tr -d '\n' | wl-copy
     notify-send -u low "up" 
 fi
