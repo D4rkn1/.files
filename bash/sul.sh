d0c40e46e6bc93e13258c9c3852e8ca4c5108764
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2025-08-17 20:29:22 +0800

    add s-ul uploader

---
+++ bash/sul.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+key=$(< $HOME/secrets/sul)
+
+if [ $# -ge 1 ]; then
+    for file in "$@"; do
+        curl -s -F "file=@$file" "https://s-ul.eu/api/v1/upload?wizard=true&key=$key" | jq -r '.url' | xargs | wl-copy
+        notify-send -u low "up" 
+    done
+else
+    slurp | grim -g - - | curl -s -F "file=@-;filename=screenshot.png" "https://s-ul.eu/api/v1/upload?wizard=true&key=$key" | jq -r '.url' | wl-copy
+    notify-send -u low "up" 
+fi
