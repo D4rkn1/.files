0ced6e35ab668439b1d0fe425a5f948f5299c3ae
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2025-10-23 07:46:21 +0800

    sul catbox: reduce jpg quality

--- bash/catbox.sh
+++ bash/catbox.sh
@@ -7,6 +7,6 @@ if [ $# -ge 1 ]; then
         notify-send -u low "up" 
     done
 else
-    slurp | grim -g - - | ffmpeg -f png_pipe -i - -q:v 2 -f image2 -vcodec mjpeg - | curl -F "reqtype=fileupload" -F "fileToUpload=@-;filename=screenshot.jpg" -F "userhash=$key" https://catbox.moe/user/api.php | wl-copy
+    slurp | grim -g - - | ffmpeg -f png_pipe -i - -q:v 4 -f image2 -vcodec mjpeg - | curl -F "reqtype=fileupload" -F "fileToUpload=@-;filename=screenshot.jpg" -F "userhash=$key" https://catbox.moe/user/api.php | wl-copy
     notify-send -u low "up" 
 fi
--- bash/sul.sh
+++ bash/sul.sh
@@ -7,6 +7,6 @@ if [ $# -ge 1 ]; then
         notify-send -u low "up" 
     done
 else
-    slurp | grim -g - - | ffmpeg -f png_pipe -i - -q:v 2 -f image2 -vcodec mjpeg - | curl -s -F "file=@-;filename=screenshot.jpg" "https://s-ul.eu/api/v1/upload?wizard=true&key=$key" | jq -r '.url' | tr -d '\n' | wl-copy
+    slurp | grim -g - - | ffmpeg -f png_pipe -i - -q:v 4 -f image2 -vcodec mjpeg - | curl -s -F "file=@-;filename=screenshot.jpg" "https://s-ul.eu/api/v1/upload?wizard=true&key=$key" | jq -r '.url' | tr -d '\n' | wl-copy
     notify-send -u low "up" 
 fi
