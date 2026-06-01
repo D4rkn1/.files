908aa019d0026f4c4f6a9d03e8f1efbc89e723d9
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-06-01 10:05:11 +0800

    d4ssh.sh: fix deprecated switch

--- bash/d4ssh.sh
+++ bash/d4ssh.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-
-$keys=$(ls | grep -v '\.pub\|config\|known_hosts\|authorized_keys\|agent')
-eval $(keychain --eval --quiet --agents ssh $keys)
+keys=$(ls $HOME/.ssh | grep -v '\.pub\|config\|known_hosts\|authorized_keys\|agent')
+eval $(keychain --eval --quiet $keys)
