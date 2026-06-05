4e72dcafb742d50c9eb5f8b3853b79c5e5cb0993
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-06-05 22:55:19 +0800

    tmux_obsidian.sh: fix tmux default to vi normal mode

--- bash/tmux_obsidian.sh
+++ bash/tmux_obsidian.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
 session=obsidian
 
 tmux new-session -d -s $session
-tmux send-key -t $session "cd ~/vaults/personal" C-m
-tmux send-key -t $session "nvim -c :ObsidianToday" C-m
+tmux send-key -t $session l "cd ~/vaults/personal" C-m
+tmux send-key -t $session l "nvim -c :ObsidianToday" C-m
 tmux a -t $session
