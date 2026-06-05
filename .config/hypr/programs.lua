a614ebcfd93b7996c3c977bb61286e1601206671
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-06-05 22:02:49 +0800

    hyprland: fix tmux_repo.sh

--- .config/hypr/programs.lua
+++ .config/hypr/programs.lua
@@ -3,4 +3,5 @@ return {
   fileManager = "thunar",
   menu = "rofi -show drun -show-icons",
   browser = "firefox",
+  repo = "tmux_repo.sh",
 }
--- bash/tmux_repo.sh
+++ bash/tmux_repo.sh
@@ -1,47 +1,51 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-touch repolist
+list_repo() {
+    fd -H -t d -g ".git" $1
+}
+
+clean_repolist() {
+    sed -e "s|^$1||" -e 's|/\.git/||g'
+}
+
+repolist_clean() {
+    list_repo "$1" | clean_repolist "$1"
+}
+
+tmux_focus() {
+    dir=$1
+    repo=$(basename $dir)
+    shift
+    cmd="[workspace 8] alacritty -e tmux a -t $repo"
+    if tmux has-session -t "$repo" 2>/dev/null; then
+        hyprctl dispatch 'hl.dsp.focus({ workspace = "8" })'
+    else
+        tmux new-session -d -s $repo
+        for key in "$@"; do
+            tmux send-key -t "$repo" l "$key" C-m
+        done
+        hyprctl eval "hl.exec_cmd('$cmd')"
+    fi
+}
+
+create_dir() {
+    if [[ ! -d "$1" ]] then
+        mkdir -p "$1"
+    fi
+}
 
 case $1 in
     find)
-        repolist=$(fd -H -t d -g ".git" ~/repo)
-
-        repolist_cleaned=$(echo "$repolist" | sed 's/\.git\///g')
-
-        # repocount=$(echo "$repolist" | wc -l)
-        #
-        selected_repo=$(echo "$repolist_cleaned" | rofi -dmenu -i -p ">>> ") 
-        if [ $? -eq 1 ]; then
-            exit 0
-        fi
-        repo=$(echo "$selected_repo" | xargs basename)
-
-        if tmux has-session -t "$repo" 2>/dev/null; then
-            hyprctl dispatch workspace 8
-        else
-            tmux new-session -d -s $repo
-            tmux send-key -t $repo "cd $selected_repo" C-m
-            tmux send-key -t $repo "vi" C-m
-            hyprctl dispatch exec "[workspace 8] alacritty -e tmux a -t $repo"
-        fi
+        repo_dir="$HOME/repo/"
+        selected=$(repolist_clean "$repo_dir" | rofi -dmenu -no-custom)
+        tmux_focus "$repo_dir$selected" "cd $repo_dir$selected" "vi"
     ;;
     create)
-        personal_path="$HOME/repo/personal/"
-        new_dir="mydir"
-        dir_input=$(rofi -dmenu -i -p "new repo name ") 
-        if [[ ! -d "$personal_path$dir_input" ]] then
-            mkdir -p "$personal_path$dir_input"
-        fi
-        repo=$(echo "$dir_input" | xargs basename)
-        if tmux has-session -t "$repo" 2>/dev/null; then
-            hyprctl dispatch workspace 8
-        else
-            tmux new-session -d -s $repo
-            tmux send-key -t $repo "cd $personal_path$dir_input" C-m
-            tmux send-key -t $repo "git init -b master" C-m
-            tmux send-key -t $repo "vi" C-m
-            hyprctl dispatch exec "[workspace 8] alacritty -e tmux a -t $repo"
-        fi
+        personal_repo_dir="$HOME/repo/personal/"
+        selected=$(repolist_clean "$personal_repo_dir" | rofi -dmenu -format f -theme-str 'inputbar { enabled: true; }')
+        create_dir "$personal_repo_dir$selected"
+        repo=$(basename $selected)
+        tmux_focus "$personal_repo_dir$selected" "cd $personal_repo_dir$selected" "git init -b master" "vi"
     ;;
     *)
         echo unknown command
