8b91d0920a887c5995eb7c225f30ec372864f3d8
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-06-15 14:04:40 +0800

    tmux_obsidian: stop opening file list on startup

--- bash/tmux_obsidian.sh
+++ bash/tmux_obsidian.sh
@@ -4,5 +4,5 @@ session=obsidian
 
 tmux new-session -d -s $session
 tmux send-key -t $session l "cd ~/vaults/personal" C-m
-tmux send-key -t $session l "nvim ." C-m
+tmux send-key -t $session l "nvim" C-m
 tmux a -t $session
