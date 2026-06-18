b161684ced3675c8f4d9e46598b989513637f675
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-06-18 14:21:24 +0800

    hyprland: fix tty-clock unused arg

--- .config/hypr/startcmd.lua
+++ .config/hypr/startcmd.lua
@@ -7,7 +7,7 @@ return {
   end,
   clock = function()
     hl.exec_cmd(
-      "alacritty -e tmux new-session -As clock 'tty-clock -c -C 6 tty-clock -f \"%a %d %B\"'",
+      "alacritty -e tmux new-session -As clock 'tty-clock -c -C 6 -f \"%a %d %B\"'",
       { workspace = "special:clock silent", fullscreen = true }
     )
   end,
