08e2a0aa1518ca2364ab92b1c12211a93490e798
D4rkn1 <73800712+D4rkn1@users.noreply.github.com>
2026-06-19 23:00:45 +0800

    hyprland: start tty-clock in locked mode

--- .config/hypr/startcmd.lua
+++ .config/hypr/startcmd.lua
@@ -7,7 +7,7 @@ return {
   end,
   clock = function()
     hl.exec_cmd(
-      "alacritty -e tmux new-session -As clock 'tty-clock -c -C 4 -S -f \"%a %d %B\"'",
+      "alacritty -e tmux new-session -As clock 'tty-clock -c -C 4 -n -f \"%a %d %B\"'",
       { workspace = "special:clock silent", fullscreen = true }
     )
   end,
